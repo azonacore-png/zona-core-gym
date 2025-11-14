66ec94cab3df40cd82a60a8b1d53559a81b7f985
azonacore-png <azonacore@gmail.com>
2025-11-14 11:16:44 -0500

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,1 +1,16 @@
+import { Html, Head, Main, NextScript } from 'next/document'
 
+export default function Document() {
+  return (
+    <Html lang="es">
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
