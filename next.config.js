5863be1a73ca6a80518a97d1fcb1af218626be91
azonacore-png <azonacore@gmail.com>
2025-11-14 18:03:33 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/zona-core-gym' : '',
+  basePath: '/zona-core-gym',
   trailingSlash: true,
   images: { unoptimized: true },
 }
