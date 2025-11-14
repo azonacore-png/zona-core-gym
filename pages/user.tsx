0ae0883000d661b509db24ebb528a3b9cf43b9b7
azonacore-png <azonacore@gmail.com>
2025-11-14 11:26:07 -0500

    Update user.tsx

--- pages/user.tsx
+++ pages/user.tsx
@@ -1,1 +1,13 @@
+import Layout from '@/components/Layout'
+import UserProgress from '@/components/UserProgress'
+import { useUser } from '@/lib/hooks'
 
+export default function UserPage() {
+  const user = useUser()
+  if (!user) return null
+  return (
+    <Layout>
+      <UserProgress userEmail={user.email} />
+    </Layout>
+  )
+}
