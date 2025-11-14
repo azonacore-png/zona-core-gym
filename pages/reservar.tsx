740a7b53a36172efc0723333bed30a7908fe7302
azonacore-png <azonacore@gmail.com>
2025-11-14 11:27:36 -0500

    Create reservar.tsx

---
+++ pages/reservar.tsx
@@ -0,0 +1,13 @@
+import Layout from '@/components/Layout'
+import ClassBooker from '@/components/ClassBooker'
+import { useUser } from '@/lib/hooks'
+
+export default function ReservarPage() {
+  const user = useUser()
+  if (!user) return null
+  return (
+    <Layout>
+      <ClassBooker userEmail={user.email} />
+    </Layout>
+  )
+}
