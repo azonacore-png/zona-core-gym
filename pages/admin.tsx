95384025287a9396a2a2caf4c3e7568496e52823
azonacore-png <azonacore@gmail.com>
2025-11-14 11:25:39 -0500

    Update admin.tsx

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,1 +1,16 @@
+import Layout from '@/components/Layout'
+import AdminDashboard from '@/components/AdminDashboard'
+import InstructorManager from '@/components/InstructorManager'
+import FinanceManager from '@/components/FinanceManager'
 
+export default function AdminPage() {
+  return (
+    <Layout>
+      <AdminDashboard />
+      <div className="grid md:grid-cols-2 gap-6 mt-6">
+        <InstructorManager />
+        <FinanceManager />
+      </div>
+    </Layout>
+  )
+}
