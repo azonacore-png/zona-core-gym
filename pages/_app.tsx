e5bbc9a5227ecbf67b24992dc19603df69b56c70
azonacore-png <azonacore@gmail.com>
2025-11-14 17:21:50 -0500

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
+// pages/_app.tsx
+import Auth from '@/components/Auth'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Auth>
+      <Component {...pageProps} />
+    </Auth>
+  )
 }
